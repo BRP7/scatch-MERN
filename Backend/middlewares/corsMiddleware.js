700755a7587a9183b5916aeb973b0bc76c2d93c5
BRP7 <bhumiparmar0007@gmail.com>
2024-08-06 04:43:26 +0530

    credentials needs to be true when setting cookies

--- Backend/middlewares/corsMiddleware.js
+++ Backend/middlewares/corsMiddleware.js
@@ -4,6 +4,7 @@ const corsOptions = {
     origin: 'http://localhost:5173', // Frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
 };
 
 export default cors(corsOptions);
