7689d19cf89953647554a5be6ff96be02cca8b52
BRP7 <bhumiparmar0007@gmail.com>
2024-08-16 22:04:10 +0530

    admin role update

--- Backend/middlewares/uploadMiddleware.js
+++ Backend/middlewares/uploadMiddleware.js
@@ -2,7 +2,6 @@
 import multer from 'multer';
 import path from 'path';
 
-// Define storage location and file naming
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
