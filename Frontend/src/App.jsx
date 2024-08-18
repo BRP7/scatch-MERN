7800399ac9a4d6fd2f1a3b08c5a094b8f14397eb
BRP7 <bhumiparmar0007@gmail.com>
2024-08-18 12:56:13 +0530

    Category Form

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,8 @@ import Register from "./components/Register";
 import Shop from "./components/Shop";
 import ThemeSwitcher from "./components/ThemeSwiter";
 import AdminProductForm from "./components/AdminProductForm";
-import Menu from "./components/Menu"; // Assuming Menu is a component you have
+import Menu from "./components/Menu";
+import CategoryForm from "./components/AdminCategoryForm";
 import "./index.css";
 import "./styles.css";
 
@@ -22,6 +23,7 @@ const AppContent = () => {
           <Route path="/profile" element={<Profile />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Login />} />
+          <Route path="/category/form" element={<CategoryForm />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Shop />} />
           <Route path="/admin/products" element={<AdminProductForm />} />
