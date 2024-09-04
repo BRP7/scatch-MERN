3ed3f47475d777ede64ff61017a432c9c5f82a01
BRP7 <bhumiparmar0007@gmail.com>
2024-09-05 03:13:26 +0530

    Context API for handling peoper authentication state after add to cart

---
+++ Frontend/src/components/AuthContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuthStatus = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/auth/status', { withCredentials: true });
+      setIsAuthenticated(response.status === 200);
+    } catch (error) {
+      setIsAuthenticated(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
